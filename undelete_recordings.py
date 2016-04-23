5951a5b5de83a896d725716af6055b455d04c554
Patrick C. McGinty <casey.mcginty@gmail.com>
2016-04-23 00:36:36 -1000

    Refactor script options to use OptParser

--- undelete_recordings.py
+++ undelete_recordings.py
@@ -16,6 +16,7 @@ from __future__ import unicode_literals
 
 from MythTV import MythDB, MythLog, MythDBError
 import sys
+from optparse import OptionParser
 
 
 def list_recs(recs):
@@ -26,62 +27,40 @@ def list_recs(recs):
               (i, rec.starttime.isoformat(), rec.title, rec.subtitle))
     return recs
 
-# Undeletes a recording. In the usual case only the recgroup and autoexpire
-# value need to be modified. Setting the deletepending value is done in event
-# that the backend expired the recording.
+
 def undelete(rec):
+    """Undeletes a recording.
+    In the usual case only the recgroup and autoexpire value need to be modified.
+    Setting the deletepending value is done in event that the backend expired the
+    recording."""
     print('undeleting ', str(rec))
-    rec.update(recgroup="Default",autoexpire=0,deletepending=0)
+    rec.update(recgroup="Default", autoexpire=0, deletepending=0)
 
-param = { 'recgroup':'Deleted' }
 
-# Valid Search Arguments:
-#   title autoexpire watched closecaptioned generic hostname category subtitle
-#   commflagged storagegroup partnumber cast transcoded duplicate chanid stars
-#   category_type parttotal livetv hdtv subtitled starttime recgroup airdate
-#   seriesid basename manualid progstart playgroup stereo showtype
-#   syndicatedepisodenumber programid
-#
-# Runtime options:
-#   --force             non-interactive, perform action on all results
-#   --verbose=LEVEL     enable verbose loggin in MythTV API
-arg = ''
-arg_list = list(sys.argv[1:])
-while len(arg_list):
-    arg = arg_list.pop(0)
-    if arg[:2] == '--':
-        arg = arg[2:]
-        if '=' in arg:
-            arg = arg.split('=', 1)
-            param[arg[0]] = arg[1]
-        else:
-            if len(arg_list):
-                arg_val = arg_list.pop(0)
-                if (arg_val[:2] == '--') or (arg_val[:1] == '-'):
-                    arg_list.insert(0, arg_val)
-                    param[arg] = ''
-                else:
-                    param[arg] = arg_val
-            else:
-                param[arg] = ''
+parser = OptionParser(usage="usage: %prog [options]")
+parser.add_option("--verbose", action="store_true", default=False,
+                  help="Enable verbose output of MythTV API.")
+parser.add_option('-f', "--force", action="store_true", default=False,
+                  help="Non-interactive mode, answer 'yes' to all questions.")
+parser.add_option('-t', "--title", action="store", type="string",
+                  help="Limit recordings that match title.")
+parser.add_option("--no-livetv", action="store_true", default=False,
+                  help="Ignore all recordings in LiveTV group.")
 
-MythLog._setlevel(param.get('verbose', 'none'))
-try:
-    param.pop('verbose')
-except:
-    pass
+opts, args = parser.parse_args()
+MythLog._setlevel( 'unknown' if opts.verbose else 'err')
 
-force = False
-if 'force' in param:
-    force = True
-    param.pop('force')
+param = {'recgroup': 'Deleted', 'title': opts.title, }
 
 try:
     recs = list(MythDB().searchRecorded(**param))
+    # FIXME: LiveTV playgroups are immediately removed from backend
+    if opts.no_livetv:
+        filter(lambda r,: r.playgroup != 'LiveTV', recs)
     if len(recs) == 0:
         print('no matching recordings found')
         sys.exit(0)
-    if force:
+    if opts.force:
         for rec in recs:
             undelete(rec)
         sys.exit(0)
@@ -94,7 +73,6 @@ except MythDBError as e:
         raise
 
 recs = dict(enumerate(recs))
-
 try:
     list_recs(recs)
     while len(recs) > 0:
